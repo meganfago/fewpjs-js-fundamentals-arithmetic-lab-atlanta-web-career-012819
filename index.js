da2f780579ccf785d00f31cd91a1d958f46576c9
Megan Fago <meganlaynefago@gmail.com>
2019-03-14 11:38:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
 // Write your code here
+
+let var1 = 31;
+let var2 = 2;
+let multiply = var1 * var2;
+let random = Math.floor(Math.random() + 1);
+let mod = 16 % 6;
+let max = Math.max(20, 2, 3, 4, 5);
